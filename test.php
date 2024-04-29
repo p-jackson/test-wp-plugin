4315539441827edf8ef36cb66acbeb6366bb8f58
Philip Jackson <p-jackson@users.noreply.github.com>
2024-04-29 22:54:20 +1200

    Create test.php

---
+++ test.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @package Test_WP_Plugin
+ * @version 1.0.0
+ */
+/*
+Plugin Name: Test WP Plugin
+Plugin URI: https://github.com/p-jackson/test-wp-plugin/
+Description: A very basic plugin for testing purposes.
+Author: Philip Jackson
+Version: 1.0.0
+Author URI: https://github.com/p-jackson
+*/
+
+function hello_dolly() {
+	$chosen = 'Hello Test WP Plugin';
+
+	printf(
+		'<p id="test-wp-plugin">%s</p>',
+		$chosen
+	);
+}
+
+add_action( 'admin_notices', 'hello_test_wp_plugin' );
